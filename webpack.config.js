50ad6c7998b3545223e56f887a9fc7eb66d71148
Penny Chang <yahusanchang@gmail.com>
2022-08-23 14:42:54 +0800

    安裝image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,26 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'img/[name].[ext]'
+            }
+          },
+          // 配置 image-webpack-loader (第一步)
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              // 只在 production 環境啟用壓縮 (第二步)
+              disable: process.env.NODE_ENV === 'production' ? false : true
+            }
+          }
+        ]
       }
     ]
   },
