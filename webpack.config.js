cff8c59a59afb9a600b0b20f04337405af9269df
Penny Chang <yahusanchang@gmail.com>
2022-08-24 14:54:34 +0800

    圖片路徑修改

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = [
           use: [
             { loader: 'file-loader',
               options: {
-                name: 'dist/images/[name].[ext]'
+                name: '[name].[ext]'
               }
             }
           ]
